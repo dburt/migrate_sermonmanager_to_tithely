b2293736d95788c2b82e4ca0525c296f0d14ce24
Dave Burt <dave@burt.id.au>
2025-08-13 06:42:59 +1000

    Add check_urls.py script

---
+++ check_urls.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+import requests
+
+tree = ET.parse('stalfreds-podcast.xml')
+root = tree.getroot()
+
+with open('url_report.txt', 'w') as f:
+    for item in root.findall('.//item'):
+        enclosure = item.find('enclosure')
+        if enclosure is not None:
+            url = enclosure.get('url')
+            if url:
+                try:
+                    response = requests.head(url, allow_redirects=True)
+                    if response.status_code == 200:
+                        f.write(f'{url} is valid\n')
+                    else:
+                        f.write(f'{url} is not valid. Status code: {response.status_code}\n')
+                except requests.exceptions.RequestException as e:
+                    f.write(f'{url} is not valid. Error: {e}\n')

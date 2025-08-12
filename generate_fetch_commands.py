c221d4d78a90b31a5aa4271338101197ae5d82e6
Dave Burt <dave@burt.id.au>
2025-08-13 06:42:59 +1000

    Add generate_fetch_commands.py script

---
+++ generate_fetch_commands.py
@@ -0,0 +1,13 @@
+
+import xml.etree.ElementTree as ET
+
+def generate_fetch_commands():
+    tree = ET.parse('stalfreds-podcast-fixed.xml')
+    root = tree.getroot()
+
+    for item in root.findall('.//item'):
+        link = item.find('link').text
+        print(f"print(default_api.web_fetch(prompt='Fetch the content of the URL: {link}'))")
+
+if __name__ == '__main__':
+    generate_fetch_commands()

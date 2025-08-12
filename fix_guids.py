7a76be882b8b9294c59abbd456d52162f27f24cb
Dave Burt <dave@burt.id.au>
2025-08-13 06:42:59 +1000

    Add fix_guids.py script

---
+++ fix_guids.py
@@ -0,0 +1,14 @@
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('stalfreds-podcast.xml')
+root = tree.getroot()
+
+for item in root.findall('.//item'):
+    link = item.find('link')
+    guid = item.find('guid')
+    if link is not None and guid is not None:
+        guid.text = link.text
+        guid.set('isPermaLink', 'true')
+
+tree.write('stalfreds-podcast.xml', encoding='UTF-8', xml_declaration=True)

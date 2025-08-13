0b4f2bbc536fa7495925021be4a04b1dc2a69b22
Dave Burt <dave@burt.id.au>
2025-08-13 22:58:57 +1000

    Fix RSS feed gen with more content

--- generate_rss_feed.py
+++ generate_rss_feed.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 from datetime import datetime, timezone
 import html
@@ -58,7 +60,7 @@ def generate_rss_feed(json_filepath, rss_filepath):
             except ValueError:
                 pub_date = ""
 
-        description = f"In this sermon, {preacher} speaks on the theme of {title} as part of the series {sermon_series}. The Bible reading is {bible_passage}. {text_content}"
+        description = f"In this sermon, {preacher} speaks on the theme of {title} as part of the series {sermon_series}. The Bible reading is {bible_passage}. {content_text}"
         summary = description
 
         enclosure = ""
